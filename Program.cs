f3498935951370184e301c60c79355b727da3741
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2023-04-12 21:03:25 +0700

    array

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System ;
+
+class demoArray {
+    static void Main (){
+        //membuat aray dari tipe char
+        char [] vokal = new char [5];
+
+        //mengisi nilai elemen array
+        vokal[0] = 'a';
+        vokal[1] = 'i';
+        vokal[2] = 'u';
+        vokal[3] = 'e';
+        vokal[4] = 'o';
+
+        //menampilkan elemen array
+        Console.WriteLine("--huruf vokal--");
+        for (int i = 0; i < 5 ; i++){
+            Console.Write(vokal[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
